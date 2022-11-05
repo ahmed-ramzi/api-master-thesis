72dec846d7b52e73a259955b5d2940d0d7c838d8
Ramzi <ramzi@dataddo.com>
2022-11-05 22:52:13 +0100

    Modified Vercel File

---
+++ build/server.js
@@ -0,0 +1,37 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var http_1 = __importDefault(require("http"));
+var body_parser_1 = __importDefault(require("body-parser"));
+var express_1 = __importDefault(require("express"));
+var logging_1 = __importDefault(require("./config/logging"));
+var config_1 = __importDefault(require("./config/config"));
+var sample_1 = __importDefault(require("./routes/sample"));
+var NAMESPACE = 'Server';
+var router = (0, express_1.default)();
+/** Parse the body of the request */
+router.use(body_parser_1.default.urlencoded({ extended: true }));
+router.use(body_parser_1.default.json());
+/** Rules of our API */
+router.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if (req.method == 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+/** Routes */
+router.use('/test', sample_1.default);
+/** Error handling */
+router.use(function (req, res, next) {
+    var error = new Error('Route not found');
+    res.status(404).json({
+        message: error.message
+    });
+});
+var httpServer = http_1.default.createServer(router);
+httpServer.listen(config_1.default.server.port, function () { return logging_1.default.info(NAMESPACE, "Server is running ".concat(config_1.default.server.hostname, ":").concat(config_1.default.server.port)); });

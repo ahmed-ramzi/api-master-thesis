dd393c46ef51f544c3c52c925a4511c4b4228eb9
Ramzi <ramzi@dataddo.com>
2022-11-07 21:07:10 +0100

    Updated welcome message

--- src/controllers/HomeController.ts
+++ src/controllers/HomeController.ts
@@ -6,7 +6,15 @@ const welcomeMessage = (req: Request, res: Response, next: NextFunction) => {
     logging.info(NAMESPACE, 'Main route called');
 
     return res.status(200).json({
-        message: 'Hello World'
+        message: 'Hello World, Welcome to my API',
+        version: 'v1',
+        author: 'Ahmed Ramzi Abdulkarm Muthana',
+        student: 'Master',
+        description: 'This is a project for my master thesis',
+        supervisor: 'Doc. Ing. Vojtěch Merunka, Ph.D.',
+        university: 'Czech University of Life Sciences Prague',
+        department: 'Information Engineering',
+        year: '2022/2023'
     });
 };
 
